2b606045e8250f2c3459bd774b4a4bf4e1914cc2
herdes <soaressantos02000@gmail.com>
2025-12-18 19:11:26 -0300

    estrutura estável antes de refatorar

---
+++ src/data/AllPersonas.js
@@ -0,0 +1,217 @@
+// Com mascara
+import dalmo from "../assets/images/personas/dalmo.webp";
+import jae from "../assets/images/personas/jae.webp";
+import aguiar from "../assets/images/personas/aguiar.webp";
+import labirinto from "../assets/images/personas/labirinto.webp";
+import kemi from "../assets/images/personas/kemi.webp";
+import henri from "../assets/images/personas/henri.webp";
+
+// Sem Mascara
+
+import colosso from "../assets/images/personas/colosso.webp";
+import x from "../assets/images/personas/x.webp";
+import mutilador from "../assets/images/personas/mutilador.webp";
+import lab from "../assets/images/personas/lab.webp";
+import fantasma from "../assets/images/personas/fantasma.webp";
+import henriCap from "../assets/images/personas/henri-capus.webp";
+
+const AllPersonas = [
+  {
+    info: "Para o agente da Ordem com quem esse assassino trocou de corpo, veja Tuco Belez.",
+    sitacao:
+      "O sangue podia até pagar bem, mas para Dalmo... a glória era viciante.",
+    interprete:
+      "Rafael “Cellbit” Lange (Natal Macabro), Richard Abelha (Hexatombe)",
+    classe: "Combatente",
+    equipe: "Mascarados",
+    status: "Morto",
+    sobre: [
+      {
+        sobre01:
+          "Dalmo Magno, também conhecido como Colosso, foi um assassino em série, um dos antagonistas do especial Natal Macabro e um dos protagonistas da série Ordem Paranormal, presente em Hexatombe.",
+      },
+      {
+        sobre02:
+          "Mais tarde, ele retorna como o Colosso, parte do culto de assassinos de que Jae-Yoon e o Mutilador Noturno participavam, ajudando a trazer pessoas até o Acampamento Lua da Benquerença. Dalmo aparece com seu traje de mergulhador para confrontar Jorge, Jorel e Breno enquanto esses lutavam contra Jae, mas é impedido ao ser atropelado por uma van da Ordem, sendo imobilizado.",
+      },
+      {
+        sobre03:
+          "Em Hexatombe, agora com o agente da Ordem Tuco Belez estando em seu corpo, Dalmo foi um dos integrantes da equipe dos Mascarados, usando como base a Mansão Abandonada. Ele acabou morrendo ao ser emboscado pelos Vampiros, tendo seu sangue completamente sugado por Velisar.",
+      },
+    ],
+    formas: [
+      {
+        name: "Dalmo Magno",
+        img: dalmo,
+      },
+      {
+        name: "Colosso",
+        img: colosso,
+      },
+    ],
+  },
+  {
+    sitacao:
+      "Ao encontrar no Sangue a liberdade da rebeldia... Jae matava porque podia.",
+    interprete:
+      "Rafael “Cellbit” Lange (Natal Macabro), Gabriela Bagi Cattuzzo (Hexatombe)",
+    classe: "Especialista",
+    equipe: "Mascarados",
+    status: "Vivo",
+    sobre: [
+      {
+        sobre01:
+          "Jae-Yoon, também conhecide apenas como Jae ou X, é uma assassina em série, um dos antagonistas do especial Natal Macabro e um dos protagonistas da série Ordem Paranormal, presente em Hexatombe.",
+      },
+      {
+        sobre02:
+          "Uma figura misteriosa que usa um capuz que cobre o seu rosto, com um X vermelho, é parceira do Mutilador Noturno e Colosso em sua caçada para assassinar pessoas na região de Inquisidor do Vale. Fez sua primeira aparição trancando um grupo de viajantes dentro da Casa Juno e fazendo eles procurarem as chaves das saídas em uma espécie de jogo macabro. Durante a sua caçada, Jae incapacitou e assassinou Ayla. Após os sobreviventes escaparem da casa, passou a espreitá-los, realizando diversos ataques surpresa para feri-los.",
+      },
+      {
+        sobre03:
+          "Após atacar e enfraquecer suas vítimas, durante uma perseguição, Jae atacou e sacrificou Mike e Lucio, matando ambos com diversas facadas. Ao fim dos acontecimentos de Natal Macabro, Jae é presa pela Ordem, após ser imobilizada e derrotada. Enquanto é contida, Jae afirma que tem um propósito muito maior do que apenas fazer suas vítimas. Em Hexatombe, Jae é um dos integrantes da equipe dos Mascarados, usando como base a Mansão Abandonada.",
+      },
+    ],
+    formas: [
+      {
+        name: "Jae-Yoon",
+        img: jae,
+      },
+      {
+        name: "X",
+        img: x,
+      },
+    ],
+  },
+  {
+    sitacao:
+      "Ao encontrar no Sangue a liberdade da rebeldia... Jae matava porque podia.",
+    interprete:
+      "Rafael “Cellbit” Lange (Natal Macabro), Cassiano Cereaw Oliveira (Hexatombe)",
+    classe: "Combatente",
+    equipe: "Mascarados",
+    status: "Vivo",
+    sobre: [
+      {
+        sobre01:
+          "Delegado Jonas Aguiar, também conhecido como Mutilador Noturno, é um assassino em série, um dos antagonistas do especial Natal Macabro e um dos protagonistas da série Ordem Paranormal, presente em Hexatombe. Ele faz um trio com X e Colosso para assassinar pessoas na região de Inquisidor do Vale, ainda que o Mutilador Noturno seja o único que é dito ser o responsável pela série de desaparecimentos na região, recebendo seu apelido por sempre caçar suas vítimas durante a noite.",
+      },
+      {
+        sobre02:
+          "Ele fez sua primeira aparição caçando os campistas dentro da Casa Juno, onde encontrou e incapacitou Ricardo, e depois matou Leandro a golpes de machado, enquanto ele tentava resgatar seu amigo. Também foi responsável pelas mortes de Lila e Nathalia, matando-as brutalmente e as jogando de um penhasco após incapacitá-las em uma emboscada.",
+      },
+      {
+        sobre03:
+          "Ao final dos acontecimentos de Natal Macabro, Aguiar é preso pela Ordem, após ser deixado a deriva no cais do lago do Acampamento. Em Hexatombe, Aguiar é um dos integrantes da equipe dos Mascarados, usando como base a Mansão Abandonada.",
+      },
+    ],
+    formas: [
+      {
+        name: "Jonas Aguiar",
+        img: aguiar,
+      },
+      {
+        name: "Mutilador Noturno",
+        img: mutilador,
+      },
+    ],
+  },
+  {
+    sitacao:
+      "Kemi não se importava com um ideal moral... ela só matava para vencer.",
+    interprete: "Beatriz Beamom Pozzebon",
+    classe: "Especialista",
+    equipe: "Mascarados",
+    status: "Vivo",
+    sobre: [
+      {
+        sobre01:
+          "Kemi, também conhecida como Fantasma, é uma das protagonistas da série Ordem Paranormal, presente em Hexatombe.",
+      },
+      {
+        sobre02:
+          "Kemi é uma mercenária, matando pessoas por dinheiro. Ela teve sua primeira aparição no final da 3ª parte de Natal Macabro, sendo mostrada apenas sua silhueta, junto dos outros assassinos. Ela teve sua aparência revelada no primeiro episódio de Hexatombe, em que aparece em seu apartamento, procurando respostas para sua perda de memória recente.",
+      },
+      {
+        sobre03:
+          "Em Hexatombe, Kemi é uma das integrantes da equipe dos Mascarados, usando como base a Mansão Abandonada.",
+      },
+    ],
+    formas: [
+      {
+        name: "Kemi",
+        img: kemi,
+      },
+      {
+        name: "Fantasma",
+        img: fantasma,
+      },
+    ],
+  },
+  {
+    sitacao:
+      "A resposta nunca esperou no final do labirinto, o que espera no final do labirinto é algo muito pior, muito mais terrível; o que espera no final do labirinto... é você.",
+    interprete: "Franco Calígrafo Madeira",
+    classe: "Ocultista",
+    equipe: "Mascarados",
+    status: "Vivo",
+    sobre: [
+      {
+        sobre01:
+          "Labirinto, também conhecido como ???, é um assassino em série e um dos protagonistas da série Ordem Paranormal, presente em Hexatombe.",
+      },
+      {
+        sobre02:
+          "Labirinto é um ocultista que possui uma intensa obsessão pelos formatos e criação de labirintos. Ele teve sua primeira aparição no final da 3ª parte de Natal Macabro, tendo sido mostrada apenas sua silhueta, junto dos outros assassinos. Ele teve sua aparência revelada no primeiro episódio de Hexatombe, em que aparece preso em uma das celas de uma delegacia na cidade de São Paulo.",
+      },
+      {
+        sobre03:
+          "No Hexatombe, Labirinto é um dos integrantes da equipe dos Mascarados, usando como base a Mansão Abandonada.",
+      },
+    ],
+    formas: [
+      {
+        name: "Labirinto",
+        img: labirinto,
+      },
+      {
+        name: "???",
+        img: lab,
+      },
+    ],
+  },
+  {
+    sitacao:
+      "Muito emocionante... mas eu avisei vocês, que vocês não iam querer me ver sangrar.",
+    classe: "Ocultista",
+    ocupacao: "Sacrifício",
+    equipe: "Mascarados",
+    status: "Vivo",
+    sobre: [
+      {
+        sobre01:
+          "Henri, cujo nome original é Lúcio Davo, é um ocultista de Sangue, um dos antagonistas e Escriptas de Kian durante Desconjuração e um dos protagonistas da série Ordem Paranormal, presente em Hexatombe.",
+      },
+      {
+        sobre02:
+          "Henri era um dos órfãos do Orfanato Santa Menefreda. Durante seu tempo no local, se tornou um dos amigos mais próximos de Gal, que o induzia a fugir junto com ele do orfanato, causando diversos problemas aos cuidadores do local.[3] Com o tempo se passando, Henri foi crescendo, sendo influenciado por Leonardo Gomes assim como a maioria das crianças do orfanato, se tornando um Escripta obcecado por sangue.",
+      },
+      {
+        sobre03:
+          "Durante o Dia Final da Desconjuração, Henri confrontou os agentes da Ordo Realitas encarregados de pará-los e Bruno, que se revelou ser um traidor. Após ser ferido gravemente e ameaçar os agentes de se matar para matar alguns deles, ele é liberado e foge, deixando os agentes prosseguirem. Henri retorna em Calamidade, dentro da cela da Contenção Obscura feita especialmente para Kian, onde ele se revela ser o atual receptáculo da Relíquia de Sangue, compartilhando sua existência com o próprio Diabo. Em Hexatombe, Henri aparece como o sacrifício da equipe dos Mascarados usando como base a Mansão Abandonada.",
+      },
+    ],
+    formas: [
+      {
+        name: "Henri",
+        img: henri,
+      },
+      {
+        name: "Henri",
+        img: henriCap,
+      },
+    ],
+  },
+];
+
+export default AllPersonas;
---
+++ src/data/personages.js
@@ -0,0 +1,197 @@
+{
+  /* Foto grande */
+}
+import dalmoIcon from "../assets/persona/aleatorias/Dalmo-icon.png";
+import jaeIcon from "../assets/persona/aleatorias/Jae-icon.png";
+import kemiIcon from "../assets/persona/aleatorias/Kemi-icon.png";
+import aguiarIcon from "../assets/persona/aleatorias/Aguiar-icon.png";
+import labirintoIcon from "../assets/persona/aleatorias/labirinto-icon.png";
+import henriIcon from "../assets/persona/aleatorias/henri-icon.png";
+
+{
+  /* Foto grande com mascaras */
+}
+import dalmoMascIcon from "../assets/persona/aleatorias/Colosso-icon.png";
+import jaeMascIcon from "../assets/persona/aleatorias/X-icon.jpg";
+import kemiMascIcon from "../assets/persona/aleatorias/-icon.png";
+import aguiarMascIcon from "../assets/persona/aleatorias/mutilador-icon.png";
+import labirintoMascIcon from "../assets/persona/aleatorias/lab-icon.png";
+
+{
+  /* Miniaturas */
+}
+import jae from "../assets/persona/outros-persona/jae-persona.jpg";
+import dalmo from "../assets/persona/outros-persona/dalmo-persona.jpg";
+import aguiar from "../assets/persona/outros-persona/aguiar-persona.jpg";
+import kemi from "../assets/persona/outros-persona/kemi-persona.jpg";
+import labirinto from "../assets/persona/outros-persona/labirinto-persona.jpg";
+import henri from "../assets/persona/outros-persona/henri.jpg";
+
+const personages = [
+  {
+    mini: dalmo,
+    text: " O sangue poderia até pagar bem, mas para Dalmo, A glória era viciante.",
+    nameGolp1: "GOLPE DE ARENA",
+    PDGolp1: "3 PD",
+    descriptionGolp1:
+      "Quando acertar um ataque corpo a corpo, você pode fazer um ataque adicional ou uma manobra.",
+    nameGolp2: "PRESSÃO ATMOSFÉRICA",
+    PDGolp2: "3 PD",
+    descriptionGolp2:
+      "Se acertar um ataque você causa +1d10 pontos de dano de energia (+5) e o alvo fica atordoado por mais uma rodada.",
+    arma1: "MANOPLAS DO COLOSSO",
+    descArma1:
+      "Esse par de manoplas amaldiçoadas de energia    faz com que cada soco seja acompanhado de pressão atmosférica demolidora. Elas causam +1d10 (+5) de dano de energia",
+    about:
+      "Dalmo… ou como a maioria prefere chamar, “o Colosso”. Se você já viu ele de perto, sabe que esse apelido não é exagero — é aviso. Dizem que ele não nasceu forte: foi moldado. Cada marca no corpo dele carrega uma história que ninguém tem coragem de pedir pra ouvir. O Colosso é o tipo de homem que avança quando todos recuam, como se o medo não tivesse lugar dentro dele. Alguns juram que ele já enfrentou criaturas do Outro Lado sozinho e voltou vivo só por teimosia. Ele não fala muito, mas quando olha pra você, parece que está avaliando se você vai aguentar o que está por vir… ou se vai ser só mais um nome nas paredes de algum ritual. Se tem alguém que você quer do seu lado quando o impossível se aproxima, é o Colosso. E se ele estiver contra você? Então é melhor correr antes que ele perceba.",
+    formas: [
+      {
+        img: dalmoIcon,
+        name: "Dalmo",
+      },
+      {
+        img: dalmoMascIcon,
+        name: "COLOSSO",
+      },
+    ],
+  },
+  {
+    mini: jae,
+    text: " Ao encontrar no sangue o ideal da rebeldia, Jae matava porque podia.",
+    nameGolp1: "ASSASSINATO FURTIVO",
+    PDGolp1: "2 PD",
+    descriptionGolp1:
+      "Quando atinge um alvo desprevinido ou que você esteja flanqueando, você causa +3d8 (12) pontos de dano.",
+    nameGolp2: "ZONA DOS SUSSURROS",
+    PDGolp2: "3 PD",
+    descriptionGolp2:
+      "Marca uma área com X. Nessa área, recebe +5 em testes de ataque, não sofre penalidade em furtividade após ações chamativas e melhora dano do assassino.",
+    arma1: "PUNHAL X",
+    descArma1:
+      "Quando atacar, você pode gastar 2PD para deixar o alvo desprevinido, se acertar o ataque, o alvo fica cego por 1 rodada.",
+    about:
+      "Jae é o tipo de pessoa que você percebe antes mesmo de ouvir o nome. Tem um olhar que parece sempre seguir alguma coisa que ninguém mais enxerga. Dizem que ele foi marcado pelo Outro Lado cedo demais, e que desde então carrega uma ligação que nenhum ritual conseguiu cortar. Ele não é dos mais fortes fisicamente, mas compensa com uma precisão quase sobrenatural: Jae nunca desperdiça um movimento, nunca hesita. Alguns acreditam que ele escuta “chamados” durante surtos de influência rituais, e que por isso sempre sabe para onde ir — como se estivesse seguindo um mapa invisível. Trabalhar com Jae é perigoso: você nunca sabe se está sendo guiado por instinto… ou por algo que usa ele como mensageiro. Mas se existe alguém capaz de encontrar uma saída no meio do caos, é ele. Ou pelo menos é isso que todo mundo espera.",
+    formas: [
+      {
+        img: jaeIcon,
+        name: "JAE-YOON",
+      },
+      {
+        img: jaeMascIcon,
+        name: "X",
+      },
+    ],
+  },
+  {
+    mini: kemi,
+    text: "Kemi não se importa com ideal moral, Ela se importa com DINHEIRO.",
+    nameGolp1: "PERITA",
+    PDGolp1: "3 PD",
+    descriptionGolp1:
+      "Quando faz teste de pericia em que é trreinada, você pode gastar 3PD para somar +1d8 no teste.",
+    nameGolp2: "DISPARO DA MORTE",
+    PDGolp2: "3 PD",
+    descriptionGolp2:
+      "Quando faz um ataque com o fuzil, você sente o tempo desacelerar, o que permite mirar com precisão letal.",
+    arma1: "SNIPER DA KEMI",
+    descArma1:
+      "A espiral que um projétel disparado por essa arma faz um simbolo de Morte: um ser reduzido a 0PV por essa arma morre em apenas dois turnos(em vez de três).",
+    about:
+      "A Kemi é o tipo de pessoa que muita gente subestima — e se arrepende depois. Ela carrega um silêncio estranho, não de quem é tímida, mas de quem ouve coisas. Coisas que você torce para nunca ouvir também. Há quem diga que a Kemi enxerga rastros do Outro Lado mesmo quando o véu está fechado. Ela não teme lugares escuros nem os símbolos que fazem outros perderem a sanidade. Na verdade, parece até confortável perto deles. É rápida, precisa e tem uma frieza que não vem da falta de emoção, mas de quem já viu demais. Alguns agentes acreditam que, se um dia o Hexatombe se abrir completamente, Kemi será uma das poucas que vai entender o que está acontecendo — talvez até antes de acontecer. Trabalhar ao lado dela dá segurança… mas também dá a sensação de que, em silêncio, ela sabe algo que você não sabe. E que talvez seja melhor assim.",
+    formas: [
+      {
+        img: kemiIcon,
+        name: "KEMI",
+      },
+      {
+        img: kemiMascIcon,
+        name: "...",
+      },
+    ],
+  },
+  {
+    mini: aguiar,
+    text: "Aguiar mentia para si mesmo que era a justiça, Mas ele matava por esporte",
+    nameGolp1: "ATAQUE ESPECIAL",
+    PDGolp1: "3 PD",
+    descriptionGolp1:
+      "Gasta PD para receber um bônus de +5 no teste de ataque e na rolagem de dado.",
+    nameGolp2: "PREDADOR DE SANGUE",
+    PDGolp2: "3 PD",
+    descriptionGolp2:
+      "Memoriza o odor de uma vitima, recebendo +1D em testes para rastreá-la, percebê-la e atacá-la.",
+    arma1: "MACHADO DO MUTILADOR NOTURNO",
+    descArma1:
+      "Quando atinge uma vitima, você pode gastar 1PD para deixá-la sangrando (um ser sangrando perde 1d6 PV por rodada).",
+    about:
+      "O Aguiar é o tipo de sujeito que não deveria estar vivo depois de tudo que já passou — mas está. E isso diz muito sobre ele. Ele não tem a força do Colosso, nem a sensibilidade ritual da Kemi, nem a precisão guiada do Jae. O que ele tem é algo mais simples… e bem mais perigoso: instinto de sobrevivência bruto, quase animal. Aguiar não recua, não congela, e não perde tempo tentando entender o que está olhando. Ele age. E, por algum motivo, essa impulsividade dele quase sempre leva ao resultado certo. Há rumores de que ele já encarou entidades do Outro Lado de frente, sem ritual, sem preparação, apenas com improviso e coragem. Nada glamuroso — só brutal. Os veteranos dizem que Aguiar não teme o Hexatombe… ou talvez ele tema tanto que aprendeu a atacá-lo antes que ele o engolisse. É difícil saber. O olhar dele é sempre firme, sempre calculando uma rota de fuga, sempre pronto para transformar qualquer coisa ao alcance em arma. Se você está numa situação impossível, cercado por criaturas que não deveriam existir, e alguém diz “o Aguiar tá vindo”… você respira um pouco mais aliviado. Mas também sabe que vai sobreviver porque ele decidiu sobreviver com você. E isso, no fim, é tudo que importa.",
+    formas: [
+      {
+        img: aguiarIcon,
+        name: "AGUIAR",
+      },
+      {
+        img: aguiarMascIcon,
+        name: "MUTILADOR NOTURNO",
+      },
+    ],
+  },
+  {
+    mini: labirinto,
+    text: "???",
+    nameGolp1: "RAJADA CAÓTICA",
+    PDGolp1: "3 PD",
+    descriptionGolp1:
+      "Você dispara um raio que causa 8d8(32) pontos de dano de Energia em um ser de alcance médio.",
+    nameGolp2: "LABIRINTO MENTAL",
+    PDGolp2: "3 PD",
+    descriptionGolp2:
+      "Com a mente presa em um labirinto, pelas próximas 1d4 rodadas, o alvo é obrigado a gastar suas ações para se mover em uma direção aleatória.",
+    nameGolp3: "CAPTURAR MOMENTO",
+    PDGolp3: "3 PD",
+    descriptionGolp3:
+      "Você marca um local com um símbulo invisível, O símbulo capta imagens e sons proxímos dele. Você pode ver ou ouvir qualquer coisa captada pelo símbulo.",
+    nameGolp4: "MAPA SANGUÍNEO",
+    PDGolp4: "3 PD",
+    descriptionGolp4:
+      "Você desenha um mapa com gotas de sangue que sinalizam em tempo real a localização de todos os seres em um raio de 1 km a partir de você.",
+    about:
+      "Ninguém sabe o verdadeiro nome dele. Entre aqueles que já viram as marcas de sangue nos corredores, ele é chamado apenas de Labirinto. Não porque ele vive em um — mas porque ele é um. Labirinto é uma entidade que distorce o espaço ao redor, dobrando salas, alongando corredores e criando caminhos impossíveis. Onde ele passa, o chão racha em padrões espirais, como se o próprio mundo estivesse tentando copiá-lo… ou fugir dele. Suas linhas são irregulares, como rabiscos feitos por alguém em desespero, sempre vermelhas, sempre frescas — como se acabassem de ser traçadas com um dedo banhado em sangue. Dizem que ele não fala. Ele guia. E nunca para um lugar seguro. A presença dele não é notada pelos olhos primeiro, mas pela sensação: um arrepio de que o caminho que você acabou de passar não existia antes. Pessoas relatam escutar um arrastar suave, como um dedo riscando paredes infinitas. Quando alguém tenta seguir essas marcas, acaba perdido por horas, dias… ou encontra apenas silêncio no final.",
+    formas: [
+      {
+        img: labirintoIcon,
+        name: "LABIRINTO",
+      },
+      {
+        img: labirintoMascIcon,
+        name: "???",
+      },
+    ],
+  },
+  {
+    mini: henri,
+    text: "??",
+    nameGolp1: "??",
+    PDGolp1: "??",
+    descriptionGolp1: "??",
+    nameGolp2: "??",
+    PDGolp2: "??",
+    descriptionGolp2: "??",
+    arma1: "??",
+    descArma1: "??",
+    about:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    formas: [
+      {
+        img: henriIcon,
+        name: "Henri",
+      },
+      {
+        img: henriIcon,
+        name: "Henri",
+      },
+    ],
+  },
+];
+
+export default personages;
